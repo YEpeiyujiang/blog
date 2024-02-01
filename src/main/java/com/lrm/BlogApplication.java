08aa4ebcb71a0e9c583b7b31ad63d037da34ec9f
YEpeiyujiang <yepeiyu2002@163.com>
2024-02-01 12:00:39 +0800

    初始化

---
+++ src/main/java/com/lrm/BlogApplication.java
@@ -0,0 +1,15 @@
+package com.lrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(BlogApplication.class, args);
+        System.out.println("服务程序已经启动");
+    }
+
+}

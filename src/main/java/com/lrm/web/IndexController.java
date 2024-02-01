2ef88f5cface06a1f9b7d9c722938c76e91ce1e9
YEpeiyujiang <yepeiyu2002@163.com>
2024-02-01 23:51:23 +0800

    日志处理

--- src/main/java/com/lrm/web/IndexController.java
+++ src/main/java/com/lrm/web/IndexController.java
@@ -11,13 +11,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
-    public String index() {
-        int i = 9/0;
-//        String blog = null;
-//        if (blog == null) {
-//            throw  new NotFoundException("博客不存在");
-//        }
+    @GetMapping("/{id}/{home}")
+    public String index(@PathVariable Integer id,@PathVariable String name) {
+        System.out.println("------------index----------");
         return "index";
     }
 

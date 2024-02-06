5678a1b522db687da828cc337503a95b3bb02f83
YEpeiyujiang <yepeiyu2002@163.com>
2024-02-06 21:40:06 +0800

    分类管理2

---
+++ src/main/java/com/lrm/web/admin/TypeController.java
@@ -0,0 +1,48 @@
+package com.lrm.web.admin;
+
+import com.lrm.po.Type;
+import com.lrm.service.TypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/admin")
+public class TypeController {
+
+    @Autowired
+    private TypeService typeService;
+
+    @GetMapping("/types")
+    public String types(@PageableDefault(size = 10,sort = {"id"},direction = Sort.Direction.DESC)
+                                    Pageable pageable, Model model) {
+        model.addAttribute("page",typeService.listType(pageable));
+        return "admin/types";
+    }
+
+    @GetMapping("/types/input")
+    public String input(Model model) {
+        model.addAttribute("type", new Type());
+        return "admin/types-input";
+    }
+    @PostMapping("/types")
+    public String post(Type type){
+        Type t = typeService.saveType(type);
+        if( t == null ){
+            //
+        }else{
+            //
+        }
+        return "redirect:/admin/types";
+    }
+
+
+
+}

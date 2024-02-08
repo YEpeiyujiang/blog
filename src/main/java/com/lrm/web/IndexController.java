dc408c9dfc1b3d06fcaca727b0e9acb711c6c6a6
YEpeiyujiang <yepeiyu2002@163.com>
2024-02-08 14:02:19 +0800

    博客展示

--- src/main/java/com/lrm/web/IndexController.java
+++ src/main/java/com/lrm/web/IndexController.java
@@ -14,9 +14,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-/**
- * Created by limi on 2017/10/13.
- */
+
 @Controller
 public class IndexController {
 
